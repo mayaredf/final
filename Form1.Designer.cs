02986bb2a2434d5c31d691433c591e360ec1ef2e
mayaredf <mayaredf850@ed.amdsb.ca>
2018-01-11 12:51:39 -0500

    h

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.fjdsfdfgu = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hpLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rivalHealthLabel = new System.Windows.Forms.Label();
+            this.userHealthLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fjdsfdfgu
+            // 
+            this.fjdsfdfgu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fjdsfdfgu.Location = new System.Drawing.Point(1, 294);
+            this.fjdsfdfgu.Name = "fjdsfdfgu";
+            this.fjdsfdfgu.Size = new System.Drawing.Size(238, 103);
+            this.fjdsfdfgu.TabIndex = 0;
+            this.fjdsfdfgu.Text = "output";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(244, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 101);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " ";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(257, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(348, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(257, 345);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(348, 345);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 35);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(220, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 48);
+            this.label2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(25, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 48);
+            this.label3.TabIndex = 7;
+            // 
+            // hpLabel
+            // 
+            this.hpLabel.AutoSize = true;
+            this.hpLabel.BackColor = System.Drawing.Color.Transparent;
+            this.hpLabel.Location = new System.Drawing.Point(348, 266);
+            this.hpLabel.Name = "hpLabel";
+            this.hpLabel.Size = new System.Drawing.Size(38, 13);
+            this.hpLabel.TabIndex = 8;
+            this.hpLabel.Text = "XX/25";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(348, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "LV 5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(165, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "LV 5";
+            // 
+            // rivalHealthLabel
+            // 
+            this.rivalHealthLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rivalHealthLabel.Location = new System.Drawing.Point(34, 67);
+            this.rivalHealthLabel.Name = "rivalHealthLabel";
+            this.rivalHealthLabel.Size = new System.Drawing.Size(150, 15);
+            this.rivalHealthLabel.TabIndex = 11;
+            // 
+            // userHealthLabel
+            // 
+            this.userHealthLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.userHealthLabel.Location = new System.Drawing.Point(229, 251);
+            this.userHealthLabel.Name = "userHealthLabel";
+            this.userHealthLabel.Size = new System.Drawing.Size(138, 15);
+            this.userHealthLabel.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 406);
+            this.Controls.Add(this.userHealthLabel);
+            this.Controls.Add(this.rivalHealthLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.hpLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fjdsfdfgu);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label fjdsfdfgu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label hpLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label rivalHealthLabel;
+        private System.Windows.Forms.Label userHealthLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,10 +11,36 @@ using System.Windows.Forms;
 namespace final
 {
     public partial class Form1 : Form
+        
     {
         public Form1()
         {
             InitializeComponent();
+            
+            int playerhp = 25;
+            int rivalhp = 25;
+            int percentHealth;
+            int accuracy;
+            int critical;
+
+            //instead of this.create graphics replace this with the label name
+            //progress bar takes values of hp
+            //if (playerhp == 0) ;
+            //if (rivalhp == 0) ;
+          
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = rivalHealthLabel.CreateGraphics();
+            Pen drawPen = new Pen(Color.LightGreen, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.LightGreen);
+            g.FillRectangle(drawBrush, 0, 0, 150, 15);
+            Graphics g2 = userHealthLabel.CreateGraphics();
+            g2.FillRectangle(drawBrush, 0, 0, 150, 15);
         }
     }
 }
