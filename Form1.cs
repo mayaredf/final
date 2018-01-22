4a1dbcd76ad601e790692da326c23accdd8a0b2c
mayaredf <mayaredf850@ed.amdsb.ca>
2018-01-22 12:36:17 -0500

    r

--- Form1.cs
+++ Form1.cs
@@ -19,8 +19,8 @@ namespace final
 
         int playerhp = 150;
         int rivalhp = 150;
-        double playeratk = 10;
-        double rivalatk = 10;
+        int playeratk = 10;
+        int rivalatk = 10;
         int accuracy;
         int critical;
         int playerbasehp = 150;
@@ -33,7 +33,7 @@ namespace final
         {
             Graphics g = rivalHealthLabel.CreateGraphics();
             Pen drawPen = new Pen(Color.LightGreen, 10);
-            SolidBrush drawBrush = new SolidBrush(Color.LightGreen);          
+            SolidBrush drawBrush = new SolidBrush(Color.LightGreen);
             Graphics g2 = userHealthLabel.CreateGraphics();
 
             accuracy = randGen.Next(1, 101);
@@ -43,12 +43,12 @@ namespace final
 
             if (accuracy <= 95)
             {
-                outputLabel.Text = ""; 
+                outputLabel.Text = "";
                 rivalhp -= playeratk;
-                
+
                 if (critical == 1)
                 {
-                    rivalhp -= playeratk * 1.5;
+                    rivalhp -= (int)(playeratk * 1.5);
                     outputLabel.Text = "A critical hit!";
                 }
             }
@@ -69,7 +69,7 @@ namespace final
 
             g.Clear(Color.Gray);
             g.FillRectangle(drawBrush, 0, 0, rivalhp, 15);
-            
+
             g2.Clear(Color.Gray);
             g2.FillRectangle(drawBrush, 0, 0, playerhp, 15);
 
@@ -77,6 +77,16 @@ namespace final
 
             Thread.Sleep(1000);
 
+            if (rivalhp > 0)
+            {
+                RivalTurn(g, drawBrush, g2);
+
+            }
+
+        }
+
+        private void RivalTurn(Graphics g, SolidBrush drawBrush, Graphics g2)
+        {
             //=====Rival's Turn=====//
             accuracy = randGen.Next(1, 101);
             critical = randGen.Next(1, 17);
@@ -89,7 +99,7 @@ namespace final
                 outputLabel.Text = "";
                 if (critical == 1)
                 {
-                    playerhp -= rivalatk * 1.5;
+                    playerhp -= (int)(rivalatk * 1.5);
                     outputLabel.Text = "A critical hit!";
                 }
             }
